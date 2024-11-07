23b8d5902f06ba637f1d23554943f2c177b14159
RELHAMRI <elh.radouane@gmail.com>
2024-11-07 11:44:10 +0100

    mise à jour

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ document.getElementById('button-clicker')
         .addEventListener('click', () =>{
     count++;
     document.getElementById('counter').innerHTML = count;
+    if(count >= 0) {
+        document.getElementById('counter').style.backgroundColor = 'olivedrab';
+        document.getElementById('counter').style.color = 'white';
+    } else {
+        document.getElementById('counter').style.backgroundColor = 'red';
+        document.getElementById('counter').style.color = 'white';
+    }
+    
 });
+
+document.getElementById('button').addEventListener('click', () => {
+    count--;
+    document.getElementById('counter').innerHTML = count;
+if(count < 0) {
+    document.getElementById('counter').style.backgroundColor = 'red';
+    document.getElementById('counter').style.color = 'white';
+} else {
+    document.getElementById('counter').style.backgroundColor = 'olivedrab';
+    document.getElementById('counter').style.color = 'white';
+}
+})
